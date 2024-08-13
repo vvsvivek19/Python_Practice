42af461e01ed727df04cccb845b2b4e982964620
Vivek Singh <vvsvivek19@gmail.com>
2024-08-13 12:14:47 +0530

    Practicing Python

--- 1_product_sum_with_condition.py
+++ 1_product_sum_with_condition.py
@@ -1,4 +1,5 @@
-#Given two integer numbers, return their product only if the product is equal to or lower than 1000. Otherwise, return their sum.
+#Given two integer numbers, return their product only if the product is equal to or lower than 1000.
+# Otherwise, return their sum.
 
 num1 = input("Enter the first number: ")
 num1 = int(num1)
