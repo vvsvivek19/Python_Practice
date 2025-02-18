b779473ff32172c834a5da8655ab0c0699c57b2b
Vivek Singh <vvsvivek19@gmail.com>
2025-02-19 04:19:15 +0530

    regex Learning

--- Regex/re-special-sequences.py
+++ Regex/re-special-sequences.py
@@ -39,3 +39,13 @@ regex_pattern = r'\D'
 result = re.findall(regex_pattern,target_string)
 print("Matching value", result)
 
+# Special Sequence\b and \B 
+import re
+target_string = """ For instance, considering the same target string, we can search for the word “ssa” using a \b special sequence like this "\bssa\b". But we will not get a match because non-alphanumeric characters do not border it on both sides"""
+regex_pattern = r"\b\w{6}\b"
+result = re.findall(regex_pattern,target_string)
+print(result)
+
+# \B
+result = re.findall(r"\Bthon", target_string)
+print(result)
--- Regex/tempCodeRunnerFile.py
+++ Regex/tempCodeRunnerFile.py
@@ -1,19 +1,9 @@
 import re
-
-target_string = "8000 dollar"
-
-#To match all digits
-regex_pattern = r'\d'
+target_string = """ For instance, considering the same target string, we can search for the word “ssa” using a \b special sequence like this "\bssa\b". But we will not get a match because non-alphanumeric characters do not border it on both sides"""
+regex_pattern = r"\b\w{6}\b"
 result = re.findall(regex_pattern,target_string)
-print("Matching value", result)
+print(result)
 
-#To match all numbers
-regex_pattern = r'\d+'
-result = re.findall(regex_pattern,target_string)
-print("Matching value", result)
-
-#To match a 4 digit number
-#To match all numbers
-regex_pattern = r'\d{4}'
-result = re.findall(regex_pattern,target_string)
-print("Matching value", result)
+# \B
+result = re.findall(r"\Bthon", target_string)
+print(result)
